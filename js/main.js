2a8c6bf9c3b979977de552755f9009452915e259
ivantsov1997 <ivantsov1997@mail.ru>
2023-10-13 19:24:24 +0500

    change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const btn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav');
 
+function noscroll() {
+    window.scrollTo(0, 0);
+}
+
 btn.addEventListener('click', ()=>{
-    nav.classList.togle('menu-open')
- })
+    nav.classList.toggle('menu-open')
+    if(nav.classList.contains('menu-open')) window.addEventListener('scroll', noscroll)
+    else window.removeEventListener('scroll', noscroll)
+})
